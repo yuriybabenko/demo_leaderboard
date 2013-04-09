47dc33b42317549af6f760d30a04e1b88afd8f58
Yuriy Babenko <yuriyb@gmail.com>
2013-04-09 13:53:05 -0700

    added static files

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Leaderboard</title>
+
+  <link rel="stylesheet" href="/css/style.css">
+
+  <script type="text/javascript" src="/js/jquery-1.9.1.min.js"></script>
+</head>
+<body>
+  <div id="outer">
+    <div class="leaderboard">
+      <div class="player">
+        <span class="name">Marie Curie</span>
+        <span class="score">123</span>
+      </div>
+    </div>
+    <div class="none">
+      Click a player to select
+    </div>
+    <div class="details">
+      <div class="name">John Smith</span>
+      <input type="button" class="inc" value="Give 5 points">
+    </div>
+  </div>
+</body>
+</html>
