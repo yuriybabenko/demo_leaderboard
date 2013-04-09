72242d9b9da8feb3cbd5c35a66f5673054863750
Yuriy Babenko <yuriyb@gmail.com>
2013-04-09 16:48:28 -0700

    wrapped up functionality; minor documentation

--- index.php
+++ index.php
@@ -1,11 +1,43 @@
 <?php
-  $base_path = 'http://bbtv/';
+  define('POINTS_VALUE',  5);
+  define('BASE_PATH',     'http://bbtv/');
 
   require_once('./includes/debug.inc');
   require_once('./includes/interface.iDataSource.inc');
+  require_once('./includes/class.Contestant.inc');
+  require_once('./includes/class.Data.inc');
   require_once('./includes/class.Session.inc');
   
-  $data = new Session();
+  // available contestants & default scores
+  $contestants = array(
+    'Marie Curie' => 0,
+    'Claude Shannon' => 0,
+    'Ada Lovelace' => 0,
+    'Nikola Tesla' => 0,
+    'Carl Friedrich Gauss' => 0,
+    'Grace Hopper' => 0,
+  );
+
+  $data = new Session($contestants);
+
+  // handle AJAX request to give a contestant points
+  if (isset($_GET['target'])) {
+    if ($existing = $data->getContestants()) {
+      if (isset($existing[$_GET['target']])) {
+        // at this point $contestant holds a copy of the identifier pointing to the 
+        // original object in session, so simply updating the object will update the
+        // session data
+        $contestant = $existing[$_GET['target']];
+
+        $score = $contestant->getScore();
+        $contestant->setScore($score + POINTS_VALUE);
+
+        // print out the new score so it can be sent back in the response
+        echo $contestant->getScore();
+      }
+    }
+    exit();
+  }
 ?>
 
 <!DOCTYPE html>
@@ -13,25 +45,31 @@
 <head>
   <title>Leaderboard</title>
 
-  <link rel="stylesheet" href="<?php echo $base_path; ?>css/style.css">
+  <link rel="stylesheet" href="<?php echo BASE_PATH; ?>css/style.css">
 
-  <script type="text/javascript" src="<?php echo $base_path; ?>js/jquery-1.9.1.min.js"></script>
-  <script type="text/javascript" src="<?php echo $base_path; ?>js/bbtv.js"></script>
+  <script type="text/javascript" src="<?php echo BASE_PATH; ?>js/jquery-1.9.1.min.js"></script>
+  <script type="text/javascript" src="<?php echo BASE_PATH; ?>js/bbtv.js"></script>
 </head>
 <body>
   <div id="outer">
     <div class="leaderboard">
-      <div class="player">
-        <span class="name">Marie Curie</span>
-        <span class="score">123</span>
-      </div>
+      <?php
+        foreach ($data->getContestants() as $key => $contestant) {
+          echo '
+                  <div class="contestant" id="' . $key . '">
+                    <span class="name">' . $contestant->getName() . '</span>
+                    <span class="score">' . $contestant->getScore() . '</span>
+                  </div>
+               ';
+        }
+      ?>
     </div>
     <div class="none">
       Click a player to select
     </div>
     <div class="details">
-      <div class="name">John Smith</span>
-      <input type="button" class="inc" value="Give 5 points">
+      <div class="name"></div>
+      <input type="button" class="inc" value="Give <?php echo POINTS_VALUE; ?> points">
     </div>
   </div>
 </body>
--- js/bbtv.js
+++ js/bbtv.js
@@ -1,3 +1,38 @@
 $(document).ready(function() {
-   
+  var base_path     = 'http://bbtv/';
+  var target = null;
+
+  $('.contestant').each(function(i, el) {
+    el = $(el);
+
+    el.click(function() {
+      // ensure the details block contains the competitor's name and is
+      // visible
+      var name = $('.name', el).html();
+      $('.details .name').html(name);
+      $('.details').css('display', 'block');
+
+      // hide the intro block
+      $('.none').css('display', 'none');
+
+      // log the target id so we can use it in the ajax call
+      target = $(el).attr('id');
+    
+      // remove selected class from all contestants & add it to the current one
+      $('.contestant').removeClass('selected');
+      el.addClass('selected');
+    });
+
+    // process input
+    $('input.inc').click(function() {
+      if (target) {
+        // run request to update the score and update with new value
+        $.ajax({
+          url: base_path + '?target=' + target
+        }).done(function(new_score) {
+          $('#' + target + ' .score').html(new_score);
+        });
+      }
+    });
+  });
 });
